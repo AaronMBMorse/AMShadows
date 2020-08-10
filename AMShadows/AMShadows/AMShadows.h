73c487fb30f1a52d052f101212b65acc16fe8a34
Aaron Morse <aaron.mbm@icloud.com>
2020-08-09 18:17:18 -0800

    Adding first bit of code

---
+++ AMShadows/AMShadows/AMShadows.h
@@ -0,0 +1,19 @@
+//
+//  AMShadows.h
+//  AMShadows
+//
+//  Created by Aaron Morse on 8/9/20.
+//  Copyright © 2020 Aaron Morse. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AMShadows.
+FOUNDATION_EXPORT double AMShadowsVersionNumber;
+
+//! Project version string for AMShadows.
+FOUNDATION_EXPORT const unsigned char AMShadowsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AMShadows/PublicHeader.h>
+
+
